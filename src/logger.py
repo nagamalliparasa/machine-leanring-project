6d67c97ac2fd49e6d673460daaf941b846246830
nagamalliparasa <nagamallirgukt@gmail.com>
2024-08-09 11:22:11 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+# for logging purpose
+
+import logging
+import os
+from datetime import datetime
+
+
+Log_file=f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+
+logs_path=os.path.join(os.getcwd(),"logs",Log_file)
+
+os.makedirs(logs_path,exist_ok=True)
+
+
+Log_file_path=os.path.join(logs_path,Log_file)
+
+
+logging.basicConfig(
+    filename=Log_file_path,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO,
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
